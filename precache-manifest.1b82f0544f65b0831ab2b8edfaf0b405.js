e0556408dd407e5e0b5f8f7f3ba667da1997fe90
Yogev Henig <yogevhenig@gmail.com>
2020-04-18 20:24:14 +0300

    Updates

---
+++ precache-manifest.1b82f0544f65b0831ab2b8edfaf0b405.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3763fa75c3639187e2d493eb884ec3ca",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "eb444d1e45fc48d59b57",
+    "url": "/facerecognitionbrain/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "082d6823bf02a6c4443a",
+    "url": "/facerecognitionbrain/static/css/main.12f09073.chunk.css"
+  },
+  {
+    "revision": "eb444d1e45fc48d59b57",
+    "url": "/facerecognitionbrain/static/js/2.47ddc151.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognitionbrain/static/js/2.47ddc151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "082d6823bf02a6c4443a",
+    "url": "/facerecognitionbrain/static/js/main.4e364ddc.chunk.js"
+  },
+  {
+    "revision": "cf6990c032c001c83906",
+    "url": "/facerecognitionbrain/static/js/runtime-main.fdc8abc9.js"
+  }
+]);
