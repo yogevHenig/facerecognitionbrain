95eae721d850c86ec49e42d054d74b3e89f04d18
Yogev Henig <yogevhenig@gmail.com>
2020-04-23 15:43:29 +0300

    nothing important

--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ class App extends Component {
           })
           .then(response => response.json())
           .then(count => {
-            this.setState(Object.assign(this.state.user, {entries: count}))
+            this.setState(Object.assign(this.state.user, { entries: count }))
           })
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
