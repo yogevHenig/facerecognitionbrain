eb6a1ab136f727e18409b8995420bbe6d68c8ea9
Yogev Henig <yogevhenig@gmail.com>
2020-04-25 13:45:48 +0300

    changes for deployment

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component	{
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/signin', {
+		fetch('https://quiet-temple-55004.herokuapp.com/signin', {
 			method: 'post',
 			headers: { 'Content-Type' : 'application/json' },
 			body: JSON.stringify({
